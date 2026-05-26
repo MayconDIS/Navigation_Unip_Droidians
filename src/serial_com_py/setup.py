800b968058a0836b22947a3d8fabbe2606697702
Maycon DIS <mdcodeprogramming@gmail.com>
2026-05-26 13:46:01 -0300

    refact: restaura firmware do Arduino e drivers Python para a versão estável do backup

--- src/serial_com_py/setup.py
+++ src/serial_com_py/setup.py
@@ -27,6 +27,8 @@ setup(
         'console_scripts': [
             'base_driver = serial_com_py.base_driver:main',
             'base_driver_pulse = serial_com_py.base_driver_pulse:main',
+            'base_driver_sim = serial_com_py.base_driver_sim:main',
+            'safe_stop = serial_com_py.safe_stop:main',
             'distance_watchdog = serial_com_py.distance_watchdog:main',
             'wasd_teleop = serial_com_py.wasd_teleop:main',
         ],
